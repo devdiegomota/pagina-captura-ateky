2aa5d3916a129affa66f617fa3a692dd914b0f8c
Diego <diego.motasocial@gmail.com>
2024-10-19 13:22:22 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -23,24 +23,48 @@ const db = getFirestore(app);
 // Chama o botão enviar
 let botao = document.getElementById('submit')
 
+
+function sumirerro() {
+    erros.erro().style.display = "none"
+    
+
+}
+
 //Função que é chamada pelo botão enviar
 async function add() {
+    
+    var nome = document.getElementById('nome').value
+    var telefone = document.getElementById('telefone').value
+    var endereco = document.getElementById('endereco').value
+    //var erro = document.getElementsByClassName('error')
 
-    try {
+    if (!nome || !telefone || !endereco ) {
+        erros.erro().style.display = "block"
 
-        const docRef = await addDoc(collection(db, "clientes"), {
+    } else {
+        try {
+            const docRef = await addDoc(collection(db, "clientes"), {
+    
+                nome: document.getElementById('nome').value,
+                telefone: document.getElementById('telefone').value,
+                endereco: document.getElementById('endereco').value
+            });
+            //Se der certo o envio dos dados
+            document.location.href = "sucess.html"
+            console.log("Document written with ID: ", docRef.id);
+    
+        } catch (e) { //Caso der erro é exibido
+            console.error("Error adding document: ", e);
+        }
 
-            nome: document.getElementById('nome').value,
-            telefone: document.getElementById('telefone').value,
-            endereco: document.getElementById('endereco').value
-        });
-        //Se der certo o envio dos dados
-        document.location.href = "sucess.html"
-        console.log("Document written with ID: ", docRef.id);
-
-    } catch (e) { //Caso der erro é exibido
-        console.error("Error adding document: ", e);
-    }
+    }  
 }
-//Observa o botão enviar ao ser clicado
-botao.addEventListener("click", add)
+//Observa os botoes e chama função
+botao.addEventListener("click", add)    
+nome.addEventListener("input", sumirerro)
+telefone.addEventListener("input", sumirerro)
+endereco.addEventListener("input", sumirerro)
+
+const erros = {
+    erro: () => document.getElementById('error')
+}
