50d4083fd438cd9b937698cdd5a692c7b86a8527
Diego Mota <diego.motasocial@gmail.com>
2024-12-19 23:12:50 -0300

    iniciei nova branch

--- login-error.js
+++ login-error.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function OnInputEmail() /* Função que chamada valida email e ativa o botão recuperar senha*/{
     AlternButtonDisable();
     AlternErrorEmail();
@@ -61,67 +60,3 @@ const form = {
     
 }
 
-=======
-function OnInputEmail() /* Função que chamada valida email e ativa o botão recuperar senha*/{
-    AlternButtonDisable();
-    AlternErrorEmail();
-}
-
-function OnInputSenha() {
-    AlternButtonDisable();
-    PasswordError();
-}
-
-function isEmailValid() /* Funçao se tiver valor no email ele chama a ValidarEmail*/  { 
-    const email = form.email().value
-    if (!email) {
-        return false;
-    } else {
-        return ValidarEmail(email) //chama a validação se tiver valor
-    }
-}
-
-function AlternErrorEmail() {
-    const email = form.email().value;
-    form.ErroEmailObrigatorio().style.display = email ? "none" : "block";
-
-    form.ErroEmailInvalido().style.display = (ValidarEmail(email)) ? "none" : "block";
-
-}
-
-function PasswordError () {
-    const password = form.Password().value
-    form.ErroSenha().style.display = password ? "none" : "block";
-}
-
-function AlternButtonDisable () {
-
-    const emailValid = isEmailValid();
-    form.ButtonRecoveryP().disabled = !emailValid /* Mantem desabilitado se falso emailValid*/
-
-const passwordValid = isPasswordValid();
-    form.LoginButton().disabled = !emailValid || !passwordValid; /* Mantem desabilitado se falso emailValid e passwordValid*/
-
-}
-
-function isPasswordValid() /*Função que verifica de tem valor no password */{
-    const password = form.Password().value
-    if (!password) {
-        return false
-    } else {
-        return true
-    }
-}
-
-const form = {
-    email: () => document.getElementById('email'),
-    ErroEmailObrigatorio: () => document.getElementById('erro-email-obrigatorio'),
-    ErroEmailInvalido: () => document.getElementById('erro-email-invalido'),
-    ErroSenha: () => document.getElementById('erro-senha'),
-    ButtonRecoveryP: () => document.getElementById('recover-password-b'),
-    LoginButton: () => document.getElementById('login-button'),
-    Password: () => document.getElementById('password'),
-    
-}
-
->>>>>>> c211a7ce82aec7ec09bf6406a8c34b17cd409b1b
